0609426a6e309fb5fe29fd157f36635913982536
Sarathchandra Reddy Kakarla <kakarlasarath4@gmail.com>
2026-07-27 23:19:48 +0530

    Introduced Auto-populated Id No on 27-07-2026

--- Admin/Student/Stu_Register.php
+++ Admin/Student/Stu_Register.php
@@ -7,6 +7,22 @@ if (!$_SESSION['Admin_Id_No']) {
   location.replace('../admin_login.php');
   </script>";
 }
+
+$next_student_id = 'VTST0001';
+$student_id_query = mysqli_query($link, "SELECT Id_No FROM student_master_data ORDER BY Id_No DESC LIMIT 1");
+
+if ($student_id_query) {
+    $student_id_row = mysqli_fetch_assoc($student_id_query);
+    $highest_student_id = $student_id_row['Id_No'] ?? '';
+
+    if (preg_match('/^VTST(\d{4})$/', $highest_student_id, $matches)) {
+        $next_student_number = (int) $matches[1] + 1;
+        $next_student_id = 'VTST' . sprintf('%04d', $next_student_number);
+    }
+}
+
+$id_no_value = isset($_POST['Id_No']) ? $_POST['Id_No'] : $next_student_id;
+
 error_reporting(0);
 ?>
 
@@ -49,7 +65,7 @@ error_reporting(0);
                 <div class="user-details">
                     <div class="input-box">
                         <span class="details">Id No. <span class="required">*</span></span>
-                        <input type="text" placeholder="Enter Id No" id="id_no" name="Id_No" oninput="this.value = this.value.toUpperCase()" required />
+                        <input type="text" placeholder="Enter Id No" id="id_no" name="Id_No" value="<?= htmlspecialchars($id_no_value) ?>" oninput="this.value = this.value.toUpperCase()" required />
                     </div>
                     <div class="input-box">
                         <span class="details">Admission No.
